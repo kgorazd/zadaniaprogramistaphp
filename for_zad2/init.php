ce15917cfbbabd94ea6d4b4917bd53e54f150a43
szrapnel <szrapnel@poczta.onet.pl>
2015-01-13 18:59:04 +0000

    rename

--- for_zad2/init.php
+++ for_zad2/init.php
@@ -2,7 +2,7 @@
   date_default_timezone_set('UTC');
   require_once('for_zad2/xml_saver.php');
   require_once('common/common.php');
-  require_once('for_zad2/save_to_file.php');
+  require_once('for_zad2/file_io.php');
   require_once('for_zad2/api/forecast_api.php');
   require_once('for_zad2/api/weather_api.php');
   require_once('for_zad2/api/api_helper.php');
